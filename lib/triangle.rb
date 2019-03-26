3663ea5ecc3419e2195ca5ad70e696e80d7168af
Ming Duan <11203duan@gmail.com>
2019-03-26 01:49:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,16 @@ def initialize(x, y, z)
 end
 
 def kind
-  if @x + @y <= @z || @size1
-    raise TriangleError
+  if x == 0 || y == 0 || z == 0
+      raise TriangleError
+    elsif x + y <= z || x + z <= y || y + z <= x
+      raise TriangleError
+    elsif x == y && y == z
+      kind = :equilateral
+    elsif x == y && x != z || y == z && y != x || x == z && x != y
+      kind = :isosceles
+    else
+      kind = :scalene
 end
 
 class TriangleError < StandardError
